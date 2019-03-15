624c6348179541ab14b54ea9797179adae33a6f8
Scott Oloier <scottoloier@gmail.com>
2019-03-15 16:21:40 -0400

    Added CORS, whitelisting later. Cleaned multer code, resolved image uploads. Added server-side file-type validation.

--- pushover-back/index.js
+++ pushover-back/index.js
@@ -2,39 +2,24 @@ require("dotenv").config()
 const path = require("path")
 const express = require("express")
 const app = express()
-const pushover = require("./controllers/pushover")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const multer = require("multer")
+const pushover = require("./controllers/pushover")
 
-app.use(bodyParser.json())
+app.use(bodyParser.json()) // required for accepting json POSTs
 app.use(express.urlencoded({extended: true }))
 app.disable("x-powered-by")
 app.set("json spaces", 2)
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*")
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-	next()
-})
+app.use(cors())
 
-const storage = multer.diskStorage({
-	destination: "./uploads",
-	filename: function (req, file, cb) {
-		const ext = null
-		switch (file.mimetype) {
-		case "image/jpeg":
-			break
-		case "image/png":
-			break
-		default:
-			return
-		}
-		cb(null, file.originalname)
-	},
-})
-
+// configure multer filenaming and filtering
 const upload = multer({
-	storage,
-	fileFilter: function (req, file, cb) {
+	storage: multer.diskStorage({
+		destination: "./uploads",
+		filename: (req, file, cb) => cb(null, file.originalname),
+	}),
+	fileFilter: (req, file, cb) => {
 		const filetypes = /jpeg|jpg|png/
 		const mimetype = filetypes.test(file.mimetype)
 		const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
@@ -45,53 +30,50 @@ const upload = multer({
 		cb("Error: File upload only supports the following filetypes - " + filetypes)
 	},
 })
+
+//
+// file upload endpoint, preceding pushover root post
+//
 app.post("/upload", upload.single("attachment"), (req, res) => {
 	res.send("success")
 })
 
-const push = new pushover({
-	user: "uJRHDRwW9oY7Y4HV8zuoxk7HXRcTk2",
-	token: "am1cbmv6x4kpirncym48pf1w63tb3w",
-	debug: true,
-	onError: (err) => {
-		console.log(err)
-	},
-})
-
-// push.send({
-// 	message: "bro",
-// 	title: "Good news!...?",
-// 	sound: "intermission",
-// 	device: "drell",
-// 	priority: 1,
-// 	file: "uploads/img.jpg",
-// })
-
+//
+// primary POST endpoint for form data to send pushover notifications
+//
 app.post("/", (req, res) => {
-
 	const message = req.body.message
 	const title = req.body.title
 	const url = req.body.url
 	const file = req.body.attachment ? `uploads/${path.basename(req.body.attachment)}` : null
 
-	const opts = {
+	const push = new pushover({
+		user: "uJRHDRwW9oY7Y4HV8zuoxk7HXRcTk2",
+		token: "am1cbmv6x4kpirncym48pf1w63tb3w",
+		// debug: true,
+		onError: (err) => {
+			console.log(err)
+		},
+	})
+
+	push.send({
 		message,
 		title,
 		url,
 		file,
 		sound: "intermission",
 		device: "drell",
-		priority: 1,
-	}
-	console.log(opts)
-	push.send(opts, (err, response) => {
+		priority: 0,
+		// priority: 2,
+		// retry: 60,
+		// expire: 500,
+	},
+	(err, response) => {
 		if (err) {
 			throw new Error(err)
 		}
 		res.json(response)
 	})
-	// save any attachments
-
 })
 
 // global error handler middleware, receives all Error exception
