2603dbde345bca56908966893038a4609b862cfa
Scott Oloier <scottoloier@gmail.com>
2019-06-05 11:10:32 -0400

    Temp removal of status codes

--- pushover-back/handlers/errorHandler.js
+++ pushover-back/handlers/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
 	console.info(`logging error code: ${err.code}`)
-	res.status(err.code || 200).json({
+	res.status(200).json({
 		message: err.message,
 	})
 	console.error(err.stack)
